afe748d3eff141224fe8456ef7fa14a26ed29d77
Martin Fernandez <Electronicsmf@aol.com>
2023-10-24 23:54:52 -0400

    connecting to the backend

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'https://localhost:7195/api/'
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
