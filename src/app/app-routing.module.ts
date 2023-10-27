e3931c0ceb1ef35231946f756e0a97dca879cda7
Martin Fernandez <Electronicsmf@aol.com>
2023-10-26 21:59:36 -0400

    Adding some css to the login and register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     loadChildren: () => import('./page/login/login.module').then( m => m.LoginPageModule)
   },
   {
